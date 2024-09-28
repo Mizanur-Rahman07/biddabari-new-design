5a2a441f5ac41c7476cd96c724c9403261f210ff
Mizanur-Rahman07 <mzrahman075@gmail.com>
2024-09-29 03:54:51 +0600

    Some Work

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,4 +28,19 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><i class="fa fa-arrow-right"></i></button>'
     });
 });
+
+$(document).ready(function(){
+    $('.blog-slide').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-arrow-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-arrow-right"></i></button>'
+    });
+});
 // ----------------- Single slick slide end ------------//
